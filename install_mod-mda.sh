27c8efa962944338a47c327550798f4deb47dbdf
Holger Wirtz <wirtz@parasitstudio.de>
2016-07-23 20:56:55 +0200

    Fixed some installation problems and added mod-mda.

---
+++ install_mod-mda.sh
@@ -0,0 +1,7 @@
+# mod-mda.lv2
+cd "${HOME}/zynthian/zynthian-sw"
+git clone https://github.com/moddevices/mda-lv2.git
+cd mda-lv2
+./waf configure --lv2-user --lv2dir=/home/pi/zynthian/zynthian-plugins/lv2
+./waf build
+sudo ./waf install
