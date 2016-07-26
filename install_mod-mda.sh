972bf83f30377d0a4d182231bec4635b17429cd2
Holger Wirtz <wirtz@kuma77>
2016-07-26 11:17:57 +0200

    Started to create a DX7-SYSEX to LV2 preset converter.

--- install_mod-mda.sh
+++ install_mod-mda.sh
@@ -4,4 +4,4 @@ git clone https://github.com/moddevices/mda-lv2.git
 cd mda-lv2
 ./waf configure --lv2-user --lv2dir=/home/pi/zynthian/zynthian-plugins/lv2
 ./waf build
-sudo ./waf install
+sudo ./waf -j1 install
