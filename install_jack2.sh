f770b49969fbe4e6f50d6cb1c52d6bcf9a1dc562
Holger Wirtz <wirtz@kuma77>
2016-09-06 16:06:37 +0200

    Fixed startup procedure with systemd

--- install_jack2.sh
+++ install_jack2.sh
@@ -1,9 +1,9 @@
 # Jack2
 cd "${HOME}/zynthian/zynthian-sw"
-sudo apt-get install -y libasound2-dev
+sudo apt-get install -y libasound2-dev libdbus-1-dev
 git clone https://github.com/jackaudio/jack2.git
 cd jack2
-./waf configure
+./waf configure --dbus --enable-pkg-config-dbus-service-dir
 ./waf build
 sudo ./waf install 
 ./waf clean
