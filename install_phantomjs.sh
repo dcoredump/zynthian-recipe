8451eb8984fc4f41d3902f94eb195363aa869333
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-09 14:49:28 +0200

    Newer version and repo for phantomjs

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,9 +1,12 @@
 # phantomjs
 cd "${HOME}/zynthian/zynthian-sw"
 sudo apt-get install -y libfontconfig
-git clone https://github.com/piksel/phantomjs-raspberrypi.git
-chmod -x phantomjs-raspberrypi/bin/phantomjs
+git clone https://github.com/fg2it/phantomjs-on-raspberry.git
+mkdir -p phantomjs-raspberrypi/bin
+cp phantomjs-on-raspberry/wheezy-jessie/v2.1.1/phantomjs phantomjs-raspberrypi/bin
+rm -rf phantomjs-on-raspberry
 chmod 775 phantomjs-raspberrypi/bin/phantomjs
+mkdir -p /home/pi/zynthian/zynthian-sw/mod-ui/phantomjs-1.9.0-linux-x86_64/bin
 sudo ln -s /home/pi/zynthian/zynthian-sw/phantomjs-raspberrypi/bin/phantomjs /usr/bin/phantomjs
 sudo ln -s /home/pi/zynthian/zynthian-sw/phantomjs-raspberrypi/bin/phantomjs /usr/local/bin/phantomjs
-mkdir -p /home/pi/zynthian/zynthian-sw/mod-ui/phantomjs-1.9.0-linux-x86_64/bin
+sudo ln -s /home/pi/zynthian/zynthian-sw/phantomjs-raspberrypi/bin/phantomjs /home/pi/zynthian/zynthian-sw/mod-ui/phantomjs-1.9.0-linux-x86_64/bin 
