fd6a8f0f61687b48a9bcd429141a01d9718adf5c
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-10 11:30:15 +0200

    Fixes...

--- install_fluidsynth.sh
+++ install_fluidsynth.sh
@@ -1,10 +1,12 @@
 # fluidsynth
 cd "${HOME}/zynthian/zynthian-sw"
-sudo apt-get install -y cmake
+sudo apt-get install -y cmake 
 sudo chown -R pi.pi "${HOME}"/.config
 git clone git://git.code.sf.net/p/fluidsynth/code-git fluidsynth-code-git
 cd fluidsynth-code-git/fluidsynth
-cmake .
+sed -i -- 's/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)\nAC_DEFINE(DEFAULT_SOUNDFONT, "share\/soundfonts\/default.sf2", \[Default soundfont\])/' configure.ac
+./autogen.sh
+./configure --disable-portaudio-support --disable-ladcca --disable-lash --disable-dart --disable-coremidi --disable-coreaudio --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-dbus-support  --disable-oss-support
 make
 sudo make install
 make clean
---
+++ install_mod-lvs-data.sh
@@ -0,0 +1,4 @@
+# mod-lv2-data
+cd "${HOME}"/zynthian/zynthian-sw
+git clone https://github.com/moddevices/mod-lv2-data.git
+cd ..
