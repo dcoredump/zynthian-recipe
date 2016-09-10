5441ff540b3a1cf1ad6452803e9d187448edd761
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-10 09:47:23 +0200

    Small changes and fixes.

--- install_fluidplug.sh
+++ install_fluidplug.sh
@@ -5,9 +5,9 @@ git clone https://github.com/falkTX/FluidPlug.git
 cd FluidPlug/
 sed -i -- 's/-ffast-math -mtune=generic -msse -msse2 -mfpmath=sse -fdata-sections -ffunction-sections/-march=armv6/' Makefile.mk
 sed -i -- 's/-msse -msse2/-march=armv6/' Makefile.mk
-sed -i -- 's/^DESTDIR =/DESTDIR =\/home\/pi\/zynthian\/zynthian-plugins/' Makefile
-sed -i -- 's/^PREFIX  = \/usr//' Makefile
-sed -i -- 's/\$(PREFIX)\/lib//' Makefile
+#sed -i -- 's/^DESTDIR =/DESTDIR =\/home\/pi\/zynthian\/zynthian-plugins/' Makefile
+#sed -i -- 's/^PREFIX  = \/usr//' Makefile
+#sed -i -- 's/\$(PREFIX)\/lib//' Makefile
 cp "${HOME}"/zynthian/zynthian-recipe/FluidPlug.Makefile .
 #sudo make -f FluidPlug.Makefile
 sudo make -f FluidPlug.Makefile
