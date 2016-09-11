a427d6c73dca71005edd002bebdb95e912a409d7
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-11 10:13:36 +0200

    Fixed a bug in sosynth

--- install_mod-lv2-data.sh
+++ install_mod-lv2-data.sh
@@ -1,11 +1,7 @@
 # install_mod-lv2-data.sh
-mkdir -p /home/pi/zynthian/zynthian-plugins/lv2
+mkdir -p /home/pi/zynthian/zynthian-plugins/mod-gui-lv2
 cd "${HOME}"/zynthian/zynthian-sw
 git clone https://github.com/moddevices/mod-lv2-data.git
 cd "${HOME}/zynthian/zynthian-sw/mod-lv2-data"
-sudo rsync -a pedalboards/* "${HOME}"/.pedalboards
-sudo rsync -a presets/* "${HOME}"/.lv2
-sudo rsync -a plugins/* "${HOME}"/zynthian/zynthian-plugins/tmp-lv2
-sudo rsync -a plugins-fixed/* "${HOME}"/zynthian/zynthian-plugins/tmp-lv2
 cd ..
 rm -rf mod-lv2-data
--- install_sosynth.sh
+++ install_sosynth.sh
@@ -2,7 +2,7 @@
 cd "${HOME}"/zynthian/zynthian-gw
 git clone https://github.com/jeremysalwen/So-synth-LV2
 cd So-synth-LV2
-sed -i -- 's/^INSTALLDIR = \$(DESTDIR)\/usr\/lib\/lv2\//INSTALLDIR = \/home\/pi\/zynthian\/zynthian-plugins\/mod-lv2/' Makefile
+sed -i -- 's/^INSTALLDIR = \$(DESTDIR)\/usr\/lib\/lv2\//INSTALLDIR = \/home\/pi\/zynthian\/zynthian-plugins\/mod-lv2\//' Makefile
 make
 sudo make install
 make clean
