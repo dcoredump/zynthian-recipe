89e8866fe2ccde84e48af8bcd6e54c070823f15f
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-11 11:18:17 +0200

    Installation of pedalboard scripts

--- install_mod_on_zynthian.sh
+++ install_mod_on_zynthian.sh
@@ -17,6 +17,9 @@ sh "${HOME}"/zynthian/zynthian-recipe/install_mod-sdk.sh
 sudo cp "${HOME}"/zynthian/zynthian-recipe/mod_zynthian/systemd/mod-host.service /etc/systemd/system
 sudo cp "${HOME}"/zynthian/zynthian-recipe/mod_zynthian/systemd/mod-ui.service /etc/systemd/system
 sudo cp "${HOME}"/zynthian/zynthian-recipe/mod_zynthian/systemd/mod-sdk.service /etc/systemd/system
+mkdir "${HOME}"/bin
+cp "${HOME}"/zynthian/zynthian-recipe/mod_zynthian/bin/pedalboard* "${HOME}"/bin
+chmod 755 "${HOME}"/bin/*
 #
 # LV2 plugins
 #
