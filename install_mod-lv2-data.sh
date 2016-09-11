fa730e927c8c669133ee7783ea7e1b579d904cc4
Holger Wirtz <wirtz@parasitstudio.de>
2016-09-11 11:14:48 +0200

    modgui automatic installer. Fixes.

--- install_mod-lv2-data.sh
+++ install_mod-lv2-data.sh
@@ -1,7 +1,29 @@
 # install_mod-lv2-data.sh
 mkdir -p /home/pi/zynthian/zynthian-plugins/mod-gui-lv2
 cd "${HOME}"/zynthian/zynthian-sw
+sudo chown -R pi.pi "${HOME}"/zynthian/zynthian-plugins
 git clone https://github.com/moddevices/mod-lv2-data.git
+for d in `ls -d "${HOME}"/zynthian/zynthian-plugins/mod-lv2/*`
+do
+        B=`basename "${d}"`
+        ls -d "${d}"/modgui >/dev/null 2>&1
+        if [ $? != 0 ]
+        then
+                ls -d "${HOME}"/zynthian/zynthian-sw/mod-lv2-data/plugins/"${B}"/modgui >/dev/null 2>&1
+                if [ $? = 0 ]
+                then
+                        rsync -avP "${HOME}"/zynthian/zynthian-sw/mod-lv2-data/plugins/"${B}"/ "${HOME}"/zynthian/zynthian-plugins/mod-lv2/"${B}"
+                        continue
+                fi
+                ls -d "${HOME}"/zynthian/zynthian-sw/mod-lv2-data/plugins-fixed/"${B}"/modgui >/dev/null 2>&1
+                if [ $? = 0 ]
+                then
+                        rsync -avP "${HOME}"/zynthian/zynthian-sw/mod-lv2-data/plugins-fixed/"${B}"/ "${HOME}"/zynthian/zynthian-plugins/mod-lv2/"${B}"
+                        continue
+                fi
+                echo "No modgui found for ${B}"
+        fi
+done
 cd "${HOME}/zynthian/zynthian-sw/mod-lv2-data"
 cd ..
 rm -rf mod-lv2-data
--- install_sosynth.sh
+++ install_sosynth.sh
@@ -1,5 +1,5 @@
 # sosynth.sh
-cd "${HOME}"/zynthian/zynthian-gw
+cd "${HOME}"/zynthian/zynthian-sw
 git clone https://github.com/jeremysalwen/So-synth-LV2
 cd So-synth-LV2
 sed -i -- 's/^INSTALLDIR = \$(DESTDIR)\/usr\/lib\/lv2\//INSTALLDIR = \/home\/pi\/zynthian\/zynthian-plugins\/mod-lv2\//' Makefile
