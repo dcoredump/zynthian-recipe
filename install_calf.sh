64f4f964e02f2acfe58c143681a3df536776caba
Holger Wirtz <dcoredump@googlemail.com>
2016-09-12 21:04:08 +0200

    SMall installation and name fix.

--- install_calf.sh
+++ install_calf.sh
@@ -6,7 +6,7 @@ cd calf
 sudo mv /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-2.0.pc /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-2.0.pc.tmp
 sudo mv /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-3.0.pc /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-3.0.pc.tmp
 sh autogen.sh
-./configure --with-lv2-dir=/home/pi/zynthian/zynthian-sw/mod-lv2 --without-lash
+./configure --with-lv2-dir=/home/pi/zynthian/zynthian-plugins/mod-lv2 --without-lash
 sudo mv /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-2.0.pc.tmp /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-2.0.pc
 sudo mv /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-3.0.pc.tmp /usr/lib/arm-linux-gnueabihf/pkgconfig/gtk+-3.0.pc
 sed -i -- 's/, 1e-20f/, (double)1e-20f/' src/analyzer.cpp
