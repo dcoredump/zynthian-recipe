1270b48c0cb25cfb833fd3419ca67a6458673773
Holger Wirtz <dcoredump@googlemail.com>
2016-11-02 14:48:36 +0000

    Started to create a recipe-installer. Fixed installation of dxsyx.

--- install_dxsyx.sh
+++ install_dxsyx.sh
@@ -3,6 +3,7 @@ cd "${HOME}/zynthian/zynthian-sw"
 git clone https://github.com/rogerallen/dxsyx.git
 cd dxsyx
 make
-sudo make install
+sudo cp bin/dxsyx /usr/local/bin
+sudo chmod 755 /usr/local/bin/dxsyx
 make clean
 cd ..
