dea6c6bd493c77147390c021ebfdddab9b5be9b0
jofemodo <fernando@zynthian.org>
2016-11-25 16:05:30 +0100

    Delete useless script

--- ch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in *.sh; do
-	sed -i -- 's/"\${HOME}\/zynthian\/zynthian\-sw"/\$ZYNTHIAN_SW_DIR/' $f
-	sed -i -- 's/"\${HOME}"\/zynthian\/zynthian\-sw/\$ZYNTHIAN_SW_DIR/' $f
-	sed -i -- 's/"\${HOME}"\/zynthian\/zynthian\-recipe/\$ZYNTHIAN_RECIPE_DIR/' $f
-done
