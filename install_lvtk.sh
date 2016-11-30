dc758a3397df9e262dcf9c0f2cc24f5781a2723e
Holger Wirtz <dcoredump@googlemail.com>
2016-11-30 16:42:31 +0100

    Added fix for avoiding output of debug information.

--- install_lvtk.sh
+++ install_lvtk.sh
@@ -2,7 +2,7 @@
 cd $ZYNTHIAN_SW_DIR
 git clone https://github.com/lvtk/lvtk.git
 cd lvtk
-./waf configure --disable-ui --disable-examples
+CFLAGS="NDEBUG=1" ./waf configure --disable-ui --disable-examples
 ./waf build
 sudo ./waf install
 ./waf clean
