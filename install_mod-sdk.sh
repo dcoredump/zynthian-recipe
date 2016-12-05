7ee99b9fcac4e1f24e69f9fc040374ce011b59b7
Holger Wirtz <dcoredump@googlemail.com>
2016-12-05 16:07:58 +0100

    Fixes for installation of mod-sdk

--- install_mod-sdk.sh
+++ install_mod-sdk.sh
@@ -1,5 +1,7 @@
 # mod-sdk
 cd $ZYNTHIAN_SW_DIR
+apt-get install -y libjpeg9-dev libzip-dev libzip-dev
+pip3 install --upgrade Pillow
 git clone --recursive https://github.com/moddevices/mod-sdk.git
 cd mod-sdk/utils
 make
