86b7512fff80378a79565de53dfbc2782c1ae3b1
Holger Wirtz <dcoredump@googlemail.com>
2017-01-22 16:29:00 +0000

    Fixes and newer version from Frank which has no dependencies to Qt for LV2 anymore.

--- install_qmidiarp.sh
+++ install_qmidiarp.sh
@@ -1,16 +1,18 @@
 # qmidiarp
 cd $ZYNTHIAN_SW_DIR/plugins
-apt-get install -y qt5-default autoconf automake libtool
+apt-get install -y autoconf automake libtool
 git clone https://github.com/emuse/qmidiarp.git
-git checkout lv2extui
 cd qmidiarp
+git checkout lv2extui
 autoreconf -i
-./configure --enable-lv2plugins --enable-qt5
+#./configure --enable-lv2plugins --enable-qt5
+./configure --prefix=/usr --enable-buildapp=no --enable-lv2pluginuis=no
 make -j4
-#sudo make install
-cp -R *.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
-cp src/.libs/qmidiarp_arp.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_arp.lv2
-cp src/.libs/qmidiarp_lfo.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_lfo.lv2
-cp src/.libs/qmidiarp_seq.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_seq.lv2
+sudo make install
+cp -R /usr/lib/lv2/qmidiarp_* $ZYNTHIAN_PLUGINS_DIR/lv2/
+#cp -R *.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
+#cp src/.libs/qmidiarp_arp.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_arp.lv2
+#cp src/.libs/qmidiarp_lfo.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_lfo.lv2
+#cp src/.libs/qmidiarp_seq.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_seq.lv2
 make clean
 cd ..
