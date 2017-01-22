9400f9b732f056f7d27680748ddbce264e975c8b
Holger Wirtz <dcoredump@googlemail.com>
2017-01-22 10:01:06 +0000

    Changed qmidiarp to avoid using of QT (not fully impelemted).

--- install_qmidiarp.sh
+++ install_qmidiarp.sh
@@ -1,14 +1,16 @@
 # qmidiarp
-cd $ZYNTHIAN_SW_DIR
-git clone git://git.code.sf.net/p/qmidiarp/code qmidiarp-code
-cd qmidiarp-code
-libtoolize --force
-aclocal
-autoheader
-automake --force-missing --add-missing
-autoconf 
-./configure --enable-lv2plugins
-make
-sudo make install
+cd $ZYNTHIAN_SW_DIR/plugins
+apt-get install -y qt5-default autoconf automake libtool
+git clone https://github.com/emuse/qmidiarp.git
+git checkout lv2extui
+cd qmidiarp
+autoreconf -i
+./configure --enable-lv2plugins --enable-qt5
+make -j4
+#sudo make install
+cp -R *.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
+cp src/.libs/qmidiarp_arp.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_arp.lv2
+cp src/.libs/qmidiarp_lfo.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_lfo.lv2
+cp src/.libs/qmidiarp_seq.so $ZYNTHIAN_PLUGINS_DIR/lv2/qmidiarp_seq.lv2
 make clean
 cd ..
