cc0fab5e2cfdb714656e5430b76b1862985492f0
root <dcoredump@googlemail.com>
2017-01-31 14:12:24 +0100

    Change from autoconf to cmake

--- recipe/fluidsynth.sh
+++ recipe/fluidsynth.sh
@@ -1,6 +1,6 @@
 # fluidsynth
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
-apt-get install -y libglib2.0-dev libtool automake autoconf libsndfile-dev
+apt-get install -y libglib2.0-dev libtool automake autoconf libsndfile-dev cmake
 cd $ZYNTHIAN_SW_DIR/plugins
 zynth_git git://git.code.sf.net/p/fluidsynth/code-git
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
@@ -11,11 +11,12 @@ then
 		ln -s code-git fluidsynth
 	fi
 	cd fluidsynth/fluidsynth
-r	sed -i -- 's/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)\nAC_DEFINE(DEFAULT_SOUNDFONT, "share\/soundfonts\/default.sf2", \[Default soundfont\])/' configure.ac
-	autoreconf -i
-	automake
-	autoreconf -i
-	./configure --disable-portaudio-support --disable-ladcca --disable-lash --disable-dart --disable-coremidi --disable-coreaudio --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-dbus-support  --disable-oss-support
+r	#sed -i -- 's/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)/AM_INIT_AUTOMAKE(fluidsynth, \$FLUIDSYNTH_VERSION)\nAC_DEFINE(DEFAULT_SOUNDFONT, "share\/soundfonts\/default.sf2", \[Default soundfont\])/' configure.ac
+	#autoreconf -i
+	#automake
+	#autoreconf -i
+	#./configure --disable-portaudio-support --disable-ladcca --disable-lash --disable-dart --disable-coremidi --disable-coreaudio --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-dbus-support  --disable-oss-support
+	cmake .
 	make
 	sudo make install
 	zynth_build_request ready
