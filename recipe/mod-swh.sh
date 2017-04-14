b87222bc1518d423de981d723c66b6da87da677c
Holger Wirtz <dcoredump@googlemail.com>
2017-04-14 14:38:06 +0100

    Fix.

--- recipe/mod-swh.sh
+++ recipe/mod-swh.sh
@@ -7,7 +7,8 @@ if [ ${?} -ne 0  -o  "${build}" = "build" ]
 then
         zynth_build_request clear
 	cd swh-lv2
-	sed -i -- 's/INSTALL_DIR_REALLY=~\/.lv2\/bundles/INSTALL_DIR_REALLY=\/home\/pi\/zynthian\/zynthian-plugins\/lv2/' Makefile
+	#sed -i -- 's/INSTALL_DIR_REALLY=~\/.lv2\/bundles/INSTALL_DIR_REALLY=\/home\/pi\/zynthian\/zynthian-plugins\/lv2/' Makefile
+	sed -i -- 's/INSTALL_DIR_REALLY=~\/.lv2\/bundles/INSTALL_DIR_REALLY=\/zynthian\/zynthian-plugins\/lv2/' Makefile
 	make
 	sudo make install-user
 	make clean
