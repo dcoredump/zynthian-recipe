f0adda05bf401358dd7354f1a4c78530ee428cf1
root <dcoredump@googlemail.com>
2017-04-26 14:58:01 +0200

    Added Zam audio tools.

---
+++ recipe/zam.sh
@@ -0,0 +1,20 @@
+# zam
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+cd $ZYNTHIAN_SW_DIR/plugins
+zynth_git https://github.com/zamaudio/zam-plugins.git
+if [ ${?} -ne 0 -o  "${build}" = "build" ]
+then
+	zynth_build_request clear
+	cd zam-plugins
+	quoted_ZYNTHIAN_PLUGINS_DIR=`quote_path ${ZYNTHIAN_PLUGINS_DIR}/lv2`
+	git submodule init
+	git submodule update
+	make
+	for i in `ls -d bin/*.lv2`
+	do
+		cp -R $i ${ZYNTHIAN_PLUGINS_DIR}/lv2
+	done
+	zynth_build_request ready
+	make clean
+	cd ..
+fi
