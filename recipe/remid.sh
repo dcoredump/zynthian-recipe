a4874b1adb35e486abbd8fc8f4cb21671c1b0546
root <dcoredump@googlemail.com>
2017-04-26 09:37:13 +0200

    Added recipe for remid.

---
+++ recipe/remid.sh
@@ -0,0 +1,18 @@
+# remid
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+cd $ZYNTHIAN_SW_DIR/plugins
+apt-get install -y cmake 
+zynth_svn https://github.com/ssj71/reMID.lv2.git reMID
+if [ ${?} -ne 0  -o  "${build}" = "build" ]
+then
+	zynth_build_request clear
+	cd reMID/trunk
+        quoted_ZYNTHIAN_PLUGINS_DIR=`quote_path ${ZYNTHIAN_PLUGINS_DIR}/lv2`
+	sed -i -- "s/DESTINATION lib\/lv2\/remid.lv2/DESTINATION ${quoted_ZYNTHIAN_PLUGINS_DIR}\/remid/" CMakeLists.txt
+	cmake -DCMAKE_INSTALL_PREFIX="${ZYNTHIAN_PLUGINS_DIR}/lv2" .
+	make
+	sudo make install
+	zynth_build_request ready
+	make clean
+	cd ../..
+fi
