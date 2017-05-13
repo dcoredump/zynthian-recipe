3601b900e92e15c3bfa7112ecb59f5db2d463db9
Holger Wirtz <dcoredump@googlemail.com>
2017-05-13 12:10:50 +0100

    Added tinygain plugin.

---
+++ recipe/tinygain.sh
@@ -0,0 +1,14 @@
+# mclk
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+cd $ZYNTHIAN_SW_DIR/plugins
+zynth_git https://github.com/x42/tinyamp.lv2.git
+if [ ${?} -ne 0 -o  "${build}" = "build" ]
+then
+        zynth_build_request clear
+	cd tinyamp.lv2
+	make
+	sudo make install LV2DIR=$ZYNTHIAN_PLUGINS_DIR/lv2
+	make clean
+	zynth_build_request ready
+	cd ..
+fi
