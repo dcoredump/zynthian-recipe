b10456353357c936e5ec15e86663e6561f1061ec
root <dcoredump@googlemail.com>
2017-05-22 10:12:23 +0200

    Added recipe for infamous plugins.

--- recipe/infamous.sh
+++ recipe/infamous.sh
@@ -1,6 +1,5 @@
 # infamous
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
-apt-get install -y ntk ntk-fluid 
 cd $ZYNTHIAN_SW_DIR/plugins
 zynth_git https://github.com/ssj71/infamousPlugins.git
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
@@ -8,14 +7,15 @@ then
 	zynth_build_request clear
 	cd infamousPlugins
 	mkdir build
-	cd build 
-	cmake -DCMAKE_INSTALL_PREFIX="${ZYNTHIAN_PLUGINS_DIR}/lv2" -DLIBDIR="" ..
+	cd build
+	cmake -DLIBDIR="" -DCMAKE_INSTALL_PREFIX=${ZYNTHIAN_PLUGINS_DIR} ..
+	for i in `find src -name cmake_install.cmake`
+	do
+		sed -i -- "s/lib\/lv2/lv2/" $i
+	done
 	make
-        sudo make install
-	rm -r ${ZYNTHIAN_PLUGINS_DIR}/lv2/lib/lv2/midigen.lv2
-	mv ${ZYNTHIAN_PLUGINS_DIR}/lv2/lib/lv2/* ${ZYNTHIAN_PLUGINS_DIR}/lv2
-	rm -r ${ZYNTHIAN_PLUGINS_DIR}/lv2/lib
-	zynth_build_request ready
+	sudo make install
+	mv /zynthian/zynthian-plugins/bin/infamous-rule /usr/local/bin
 	make clean
-	cd ../..
+	cd ..
 fi
