5877a82d6583e039f31192ebdd73949551c255a3
Holger Wirtz <dcoredump@googlemail.com>
2017-07-17 12:51:30 +0100

    kx

--- recipe/helm.sh
+++ recipe/helm.sh
@@ -1,14 +1,16 @@
 # helm
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 cd $ZYNTHIAN_SW_DIR/plugins
-sudo apt-get install -y libxinerama-dev libxcursor-dev libcurl4-dev
+#sudo apt-get install -y libxinerama-dev libxcursor-dev libcurl4-openssl-dev
+sudo apt-get install -y libxinerama-dev libcurl4-openssl-dev
 zynth_git https://github.com/mtytel/helm.git
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
 then
 	zynth_build_request clear
 	cd helm
-	export DEBCXXFLAGS="${CXXFLAGS}"
-	find . -type f -name "Makefile*" -exec sed -i -- "s/-msse2/${CPU} ${FPU}/" {} \;
+	#export DEBCXXFLAGS="${CXXFLAGS}"
+	#find . -type f -name "Makefile*" -exec sed -i -- "s/-msse2/${CPU} ${FPU}/" {} \;
+	sed -i -- "s/-march=armv8-a -mtune=cortex-a53/${CPU} ${FPU}/" Makefile
 	make lv2
 	sudo make install
 	ln -s /usr/lib/lv2/helm.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
