207f8901c7df1587eab6c8b80d73479baf2dd4b6
root <dcoredump@googlemail.com>
2017-07-17 11:31:22 +0200

    Working with helm standalone installation.

--- recipe/helm.sh
+++ recipe/helm.sh
@@ -1,7 +1,7 @@
 # helm
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 cd $ZYNTHIAN_SW_DIR/plugins
-sudo apt-get install -y libxinerama-dev libxcursor-dev
+sudo apt-get install -y libxinerama-dev libxcursor-dev libcurl4-dev
 zynth_git https://github.com/mtytel/helm.git
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
 then
@@ -9,7 +9,7 @@ then
 	cd helm
 	export DEBCXXFLAGS="${CXXFLAGS}"
 	find . -type f -name "Makefile*" -exec sed -i -- "s/-msse2/${CPU} ${FPU}/" {} \;
-	make
+	make lv2
 	sudo make install
 	ln -s /usr/lib/lv2/helm.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
 	zynth_build_request ready
