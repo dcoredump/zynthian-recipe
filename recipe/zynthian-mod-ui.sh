ff9fe5013eeebd0ae986b3eb2750acae1ab67ae4
root <dcoredump@googlemail.com>
2017-09-07 08:51:32 +0200

    Using autostatic repos for jack2.

---
+++ recipe/zynthian-mod-ui.sh
@@ -0,0 +1,17 @@
+# zynthian-mod-ui
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+sudo apt-get install -y python3-pip
+cd $ZYNTHIAN_SW_DIR
+zynth_git_recursive https://github.com/zynthian/mod-ui.git
+if [ ${?} -ne 0 -o  "${build}" = "build" ]
+then
+	zynth_build_request clear
+	cd mod-ui
+	sudo -H pip3 install -r requirements.txt
+	#pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
+	#pip3 install --upgrade Pillow
+	cd utils
+	make
+	zynth_build_request ready
+	cd ../..
+fi
