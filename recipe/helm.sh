e526d35640005b3dd917f168f2718dc3d8bb3a87
root <dcoredump@googlemail.com>
2017-09-08 14:44:02 +0200

    Fix.

--- recipe/helm.sh
+++ recipe/helm.sh
@@ -1,7 +1,7 @@
 # helm
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 cd $ZYNTHIAN_SW_DIR/plugins
-sudo apt-get install -y libxcursor-dev libxinerama-dev libcurl4-openssl-dev
+sudo apt-get install -y libxcursor-dev libxinerama-dev libcurl4-openssl-dev mesa-common-dev
 zynth_git https://github.com/mtytel/helm.git
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
 then
@@ -11,7 +11,7 @@ then
 	sed -i -- "s/PDEBCXXFLAGS := \$(shell dpkg-buildflags --get CXXFLAGS)/PDEBCXXFLAGS = -O2 -fstack-protector-strong -Wformat -Werror=format-security ${CPU} ${FPU}/" Makefile
 	sed -i -- "s/-march=armv8-a -mtune=cortex-a53/${CPU} ${FPU}/" Makefile
 	make lv2
-	#sudo make install
+	sudo make install
 	mkdir -p "${ZYNTHIAN_PLUGINS_DIR}"/lv2/helm.lv2
 	cp builds/linux/LV2/helm.lv2/* "${ZYNTHIAN_PLUGINS_DIR}"/lv2/helm.lv2
 	zynth_build_request ready
