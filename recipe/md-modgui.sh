e23a2aa6f1adaa61efc725670b1361625ff1bdd9
Holger Wirtz <dcoredump@googlemail.com>
2017-09-15 12:11:36 +0100

    First version.

--- recipe/md-modgui.sh
+++ recipe/md-modgui.sh
@@ -6,9 +6,28 @@ find . -maxdepth 2 -type d -name "modgui" | while read p
 do
 	plugin_path_name=`echo "${p}"|cut -d"/" -f2`
 	echo -n "${plugin_path_name} "
-	if [ -d "${ZYNTHIAN_PLUGIN_PATH}/lv2/${plugin_path_name}" ]
+	if [ -e "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}" ]
 	then
-		echo "exists."
+		echo -n "exists "
+		if [ ! -e "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/modgui" ]
+		then
+			echo "and can be gui-fied."
+			if [ `grep modguis.ttl "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl" | wc -l` -eq 0 ]
+			then
+				echo "* Adding modgui.ttl to manifest.ttl"
+				cp "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl" "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl.bak"
+				sed -i -- 's/rdfs:seeAlso/rdfs:seeAlso <modguis.ttl>, /' "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl"
+			elif [ `grep modgui.ttl "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl" | wc -l` -eq 0 ]
+			then
+				echo "* Adding modgui.ttl to manifest.ttl"
+				cp "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl" "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl.bak"
+				sed -i -- 's/rdfs:seeAlso/rdfs:seeAlso <modgui.ttl>, /' "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}/manifest.ttl"
+			fi
+			echo "* copying modgui dir"
+			cp -R "${plugin_path_name}"/modgui* "${ZYNTHIAN_PLUGINS_DIR}/lv2/${plugin_path_name}"
+		else
+			echo "and has already a gui."
+		fi
 	else
 		echo "does not exist"
 	fi
