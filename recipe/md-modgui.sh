fa129e1b3998d34cf60533cb13e96a439c485562
root <dcoredump@googlemail.com>
2017-09-15 10:41:50 +0200

    .

--- recipe/md-modgui.sh
+++ recipe/md-modgui.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 MD_PATH="${HOME}/tmp/MOD/root-mod-duo/.lv2"
+
+cd "${MD_PATH}"
+find . -maxdepth 2 -type d -name "modgui" | while read p
+do
+	plugin_path_name=`echo "${p}"|cut -d"/" -f2`
+	echo -n "${plugin_path_name} "
+	if [ -d "${ZYNTHIAN_PLUGIN_PATH}/lv2/${plugin_path_name}" ]
+	then
+		echo "exists."
+	else
+		echo "does not exist"
+	fi
+done
