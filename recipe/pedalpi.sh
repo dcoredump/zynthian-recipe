2d37bd80e8f67ac8e598164edb865341fc21be3f
root <dcoredump@googlemail.com>
2017-09-29 14:13:25 +0200

    Fix.

--- recipe/pedalpi.sh
+++ recipe/pedalpi.sh
@@ -1,8 +1,6 @@
 # pedalpi.sh
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 cd $ZYNTHIAN_SW_DIR
-if [ ${?} -ne 0 -o  "${build}" = "build" ]
-then
-	zynth_build_request clear
-	pip3 install PedalPi-PluginsManager
-fi
+zynth_build_request clear
+pip3 install PedalPi-PluginsManager
+zynth_build_request ready
