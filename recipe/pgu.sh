6a8e4ab07861cec43815965ca7adf3f4ca072ed2
root <dcoredump@googlemail.com>
2017-10-04 14:41:49 +0200

    Added pgu.

---
+++ recipe/pgu.sh
@@ -0,0 +1,13 @@
+# mod-ui
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+sudo apt-get install python3-pygame
+cd $ZYNTHIAN_SW_DIR
+zynth_git_recursive https://github.com/dcoredump/pgu.git
+if [ ${?} -ne 0 -o  "${build}" = "build" ]
+then
+	zynth_build_request clear
+	cd pgu
+	python3 setup.py install
+	zynth_build_request ready
+	cd ..
+fi
