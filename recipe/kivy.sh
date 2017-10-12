9cd330ff6633bfbe3a240126da50e9b248eaf28c
Holger Wirtz <wirtz@parasitstudio.de>
2017-10-12 15:21:10 +0200

    Fix.

--- recipe/kivy.sh
+++ recipe/kivy.sh
@@ -7,16 +7,17 @@ sudo apt-get install -y --no-install-recommends pkg-config \
    gstreamer1.0-{omx,alsa} python3-dev libraspberrypi-dev \
    python3-setuptools-git
 pip3 install --upgrade Cython==0.25.2
-#pip3 install git+https://github.com/kivy/kivy.git@master
 cd $ZYNTHIAN_SW_DIR
 zynth_git https://github.com/kivy/kivy
 if [ ${?} -ne 0 -o  "${build}" = "build" ]
 then
         zynth_build_request clear
+	#pip3 install git+https://github.com/kivy/kivy.git@master
 	cd kivy
-	sed -i -- "s/c_options\['use_egl'\] = .+/c_options\['use_egl'\] = True/"
-	python3 setup.py build
+	sed -i -- "s/c_options\['use_egl'\] = .+/c_options\['use_egl'\] = True/" setup.py
+	python3 setup.py build && python3 setup.py install
         zynth_build_request ready
+	python3 setup.py clean
         cd ..
 fi
 #KIVY_BCM_DISPMANX_ID=2 python3 main.py 
