3b05c928d368b08fd2bb02f4fde5eead69e85b71
Holger Wirtz <wirtz@parasitstudio.de>
2017-10-12 15:05:40 +0200

    Fix.

--- recipe/kivy.sh
+++ recipe/kivy.sh
@@ -1,14 +1,22 @@
 # kivy
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
-apt purge cython3
-apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
-   pkg-config libgl1-mesa-dev libgles2-mesa-dev \
-   python3-setuptools-git libgstreamer1.0-dev git-core \
+sudo apt-get purge cython3
+sudo apt-get install -y --no-install-recommends pkg-config \
+   libgstreamer1.0-dev git-core \
    gstreamer1.0-plugins-{bad,base,good,ugly} \
-   gstreamer1.0-{omx,alsa} python-dev
-#wget https://bootstrap.pypa.io/get-pip.py
-#cp /usr/bin/pip3 /usr/bin/pip3.orig
-#python3 get-pip.py
-#ln -s /usr/local/bin/pip /usr/bin/pip3
+   gstreamer1.0-{omx,alsa} python3-dev libraspberrypi-dev \
+   python3-setuptools-git
 pip3 install --upgrade Cython==0.25.2
-pip3 install git+https://github.com/kivy/kivy.git@master
+#pip3 install git+https://github.com/kivy/kivy.git@master
+cd $ZYNTHIAN_SW_DIR
+zynth_git https://github.com/kivy/kivy
+if [ ${?} -ne 0 -o  "${build}" = "build" ]
+then
+        zynth_build_request clear
+	cd kivy
+	sed -i -- "s/c_options\['use_egl'\] = .+/c_options\['use_egl'\] = True/"
+	python3 setup.py build
+        zynth_build_request ready
+        cd ..
+fi
+#KIVY_BCM_DISPMANX_ID=2 python3 main.py 
