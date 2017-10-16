5db54e9b0e0e386f70358ce9ddbf4ebd3bf2ccbe
Holger Wirtz <wirtz@parasitstudio.de>
2017-10-16 18:18:22 +0200

    Added installation logging to /usr/local/tmp/log.

--- zynthian-stage/installer.sh
+++ zynthian-stage/installer.sh
@@ -30,7 +30,7 @@ then
     fi
     echo `date` >  ~/.wiggled
     echo ". /zynthian/zynthian-recipe/zynthian_envars.sh" >> "${HOME}/.bashrc"
-    echo "/usr/bin/screen -S install /zynthian/zynthian-recipe/zynthian-stage/setup.sh 2>&1" >> "${HOME}/.bashrc"
+    echo "/usr/bin/screen -S install /zynthian/zynthian-recipe/zynthian-stage/setup.sh 2>&1 | tee /usr/local/tmp/log" >> "${HOME}/.bashrc"
     touch "${HOME}/.install-stage1"
     bash /zynthian/zynthian-recipe/rpi-wiggle.sh
 fi
