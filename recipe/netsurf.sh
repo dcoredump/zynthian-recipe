12d70c018e8c4ee5ef7fb8a33a0ab5043390afa2
Holger Wirtz <wirtz@parasitstudio.de>
2017-12-07 12:22:25 +0100

    Fix.

--- recipe/netsurf.sh
+++ recipe/netsurf.sh
@@ -3,12 +3,15 @@ mkdir -p ${HOME}/netsurf/workspace
 cd ${HOME}/netsurf/workspace
 mkdir inst
 cat > env.sh <<'EOF'
-export PKG_CONFIG_PATH=${HOME}/netsurf/workspace/inst/lib/pkgconfig::
+export PKG_CONFIG_PATH=${HOME}/netsurf/workspace/inst/lib/pkgconfig
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/netsurf/workspace/inst/lib
 export PREFIX=${HOME}/netsurf/workspace/inst
 export PATH="${PREFIX}/bin:${PATH}"
 EOF
-source env.sh
+export PKG_CONFIG_PATH=${HOME}/netsurf/workspace/inst/lib/pkgconfig
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/netsurf/workspace/inst/lib
+export PREFIX=${HOME}/netsurf/workspace/inst
+export PATH="${PREFIX}/bin:${PATH}"
 for i in buildsystem libparserutils libwapcaplet libhubbub libcss libnsgif libnsbmp librosprite libnsfb libdom libnsutils libutf8proc nsgenbind
 do
 	git clone --recursive git://git.netsurf-browser.org/$i
