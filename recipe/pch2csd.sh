364aba7da0020c7c5192885734af3710915e7130
Holger Wirtz <holger@zynthian.org>
2018-02-15 12:52:37 +0000

    Fixes.

--- recipe/pch2csd.sh
+++ recipe/pch2csd.sh
@@ -1,6 +1,6 @@
 # pch2csd.sh
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
-#sudo apt-get install -y --no-install-recommends 
+sudo apt-get install -y --no-install-recommends 
 cd $ZYNTHIAN_SW_DIR/plugins
 zynth_git https://github.com/gleb812/pch2csd.git
 if [ "${?}" -ne 0 -o "x${build}" != "x" ]
@@ -8,6 +8,8 @@ then
 	zynth_build_request clear
 	cd pch2csd
 	sudo python3 setup.py install
+	sudo pip3 install pch2csd
+	sudo pip3 install --upgrade typing
 	zynth_build_request ready 
 	cd ..
 fi
