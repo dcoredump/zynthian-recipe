33382fa170eac940f2c9c8df50a05a473abfbebe
Holger Wirtz <holger@zynthian.org>
2018-02-15 10:18:42 +0000

    csound.sh added.

---
+++ recipe/csound.sh
@@ -0,0 +1,24 @@
+# csound
+. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
+DEBIAN_VERSION=`lsb_release -a 2>/dev/null | grep "Codename:" | cut -f2`
+echo "deb-src http://mirrordirector.raspbian.org/raspbian/ $DEBIAN_VERSION main contrib non-free rpi" > /etc/apt/sources.list.d/sources.list
+sudo apt-get update
+sudo apt-get build-dep csound
+apt-get install swig python3-dev
+export PYTHONPATH=/usr/local/lib
+zynth_git https://github.com/csound/csound.git
+if [ "${?}" -ne 0 -o "x${build}" != "x" ]
+then
+	zynth_build_request clear
+	cd csound
+	cp Custom.cmake.ex Custom.cmake
+	mkdir cs6make
+	cd cs6make
+	cmake .. -DBUILD_CSOUND_AC=OFF -DCMAKE_BUILD_TYPE="Release"
+	make
+	sudo make install
+	sudo ldconfig
+	zynth_build_request ready 
+	make clean
+	cd ../..
+fi
