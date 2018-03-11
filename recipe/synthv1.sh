bbfe796bbb3db20fbdee1600688258a5582f2fa5
Holger Wirtz <holger@zynthian.org>
2018-03-11 17:47:15 +0000

    Renamed padthv1 and to synthv1 and added padthv1.sh.

--- recipe/synthv1.sh
+++ recipe/synthv1.sh
@@ -1,25 +1,25 @@
-# padthv1.lv2
+# synthv1.lv2
 . $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 sudo apt-get install -y --no-install-recommends qt5-qmake qt5-default
 cd $ZYNTHIAN_SW_DIR/plugins
-zynth_git https://github.com/rncbc/padthv1.git
+zynth_git https://github.com/rncbc/synthv1.git
 if [ "${?}" -ne 0 -o "x${build}" != "x" ]
 then
 	zynth_build_request clear
-	cd padthv1
+	cd synthv1
         libtoolize --force
         aclocal
         autoheader
         automake --force-missing --add-missing
         autoconf 
-	./configure
+	./configure --with-lv2=/zynthian/zynthian-plugins/lv2
 	if [ "${build}" = "clean" ]
 	then
 		make clean
 	fi
 	make
 	sudo make install
-	mv /usr/local/lib/lv2/padtv1.lv2/ /zynthian/zynthian-plugins/lv2
+	mv /usr/local/lib/lv2/synthv1.lv2 /zynthian/zynthian-plugins/lv2
 	zynth_build_request ready 
 	make clean
 	cd ..
