011dce6e15311f6401d5d55a022ba168575d8130
Holger Wirtz <dcoredump@googlemail.com>
2019-07-04 16:57:04 +0100

    .

--- recipe/mod-classic-midi-sequencer.sh
+++ recipe/mod-classic-midi-sequencer.sh
@@ -8,16 +8,19 @@ then
 	zynth_build_request clear
 	cd mod-classic-midi-sequencer
 	git checkout dev_Bram
-	cd source
 	if [ "${build}" = "clean" ]
 	then
 		make clean
 	fi
-	quoted_ZYNTHIAN_PLUGINS_DIR=`quote_path ${ZYNTHIAN_PLUGINS_DIR}/lv2`
-	sed -i -- "s/^INSTALL_PATH = \/usr\/local\/lib\/lv2/INSTALL_PATH = $quoted_ZYNTHIAN_PLUGINS_DIR/" Makefile
-	sed -i -- "s/\$(DESTDIR)\$(PREFIX)\/lib\/lv2/$quoted_ZYNTHIAN_PLUGINS_DIR/" Makefile
+	#quoted_ZYNTHIAN_PLUGINS_DIR=`quote_path ${ZYNTHIAN_PLUGINS_DIR}/lv2`
+	#sed -i -- "s/^INSTALL_PATH = \/usr\/local\/lib\/lv2/INSTALL_PATH = $quoted_ZYNTHIAN_PLUGINS_DIR/" Makefile
+	#sed -i -- "s/\$(DESTDIR)\$(PREFIX)\/lib\/lv2/$quoted_ZYNTHIAN_PLUGINS_DIR/" Makefile
 	make
-	sudo make install
+	#sudo make install
+	cd source
+	cp -R step-sequencer/source/Step-Sequencer.lv2 ${ZYNTHIAN_PLUGINS_DIR}/lv2
+	cp -R phrase-sequencer/source/Phrase-Sequencer.lv2 ${ZYNTHIAN_PLUGINS_DIR}/lv2
+	cp -R drum-sequencer/source/Drum-Sequencer.lv2 ${ZYNTHIAN_PLUGINS_DIR}/lv2
 	zynth_build_request ready 
 	make clean
 	cd ../..
